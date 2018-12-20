5737ca8e2f4f9584a71b09dd32fb93905c50474c
Suneel <sunikulk0208@gmail.com>
2018-12-20 14:13:29 +0000

    test

--- comp.sh
+++ comp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/centos/.local/bin:/home/centos/bin
-cobc -x intro1.cbl
+for i in *.cbl; do
+  outf=`echo $i | awk -F. '{print $1}'`
+# echo "(cobc -x $i -o /home/centos/suneelcobolmasterbranch/$outf"
+  x=$(cobc -x $i -o /home/centos/suneelcobolmasterbranch/$outf);
+done
